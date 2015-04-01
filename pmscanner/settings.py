20ddb863abda4b1009f7171b8d8498a814c24acc
gorio <gmcbuencamino@gmail.com>
2015-04-02 01:42:05 +0800

    Initialized project.

---
+++ pmscanner/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for pmscanner project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'pmscanner'
+
+SPIDER_MODULES = ['pmscanner.spiders']
+NEWSPIDER_MODULE = 'pmscanner.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'pmscanner (+http://www.yourdomain.com)'
