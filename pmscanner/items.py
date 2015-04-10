98d2a43c44228a91a3445fe0e45633b8a2422396
gorio <gmcbuencamino@gmail.com>
2015-04-11 02:10:03 +0800

    Working brand, link, and date posted.

--- pmscanner/items.py
+++ pmscanner/items.py
@@ -12,3 +12,11 @@ class PmscannerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class Guitar(scrapy.Item):
+	brand = scrapy.Field()
+	model = scrapy.Field()
+	price = scrapy.Field()
+	contact = scrapy.Field()
+	link = scrapy.Field()
+	date_posted = scrapy.Field()
---
+++ pmscanner/spiders/guitars_spider.py
@@ -0,0 +1,80 @@
+import scrapy
+import os
+from scrapy.spider import Spider
+from scrapy.http import Request
+from scrapy.contrib.loader import ItemLoader
+import re
+from pmscanner.items import Guitar
+from datetime import datetime,timedelta
+
+class GuitarsSpider(Spider):
+	name = 'guitars'
+	allowed_domains = ['talk.philmusic.com']
+	start_urls = [
+		"http://talk.philmusic.com/index.php?board=167.0"
+	]
+
+	def __init__(self,brand='',model='',pages='',forsale='',*args,**kwargs):
+		self.brand = brand
+		self.model = model
+		self.pages = pages
+		self.forsale=forsale
+		self.brandchoices=[]
+		with open("brands.txt","r") as f:
+			for line in f:
+				self.brandchoices.append(line.strip())
+
+		if self.pages:
+			for pagenumber in range(0,int(self.pages)*50,50)[1:]:
+				self.start_urls.append(
+					'http://talk.philmusic.com/index.php?board=167.%s' % str(pagenumber)
+				)
+
+	def parse(self,response):
+		for url in response.xpath('//td[@class="subject windowbg2"]/div/span/a/@href').extract():
+			yield(Request(
+				url=url,
+				callback=self.parse_item
+				)
+			)
+
+	def parse_item(self,response):
+		item = Guitar()
+		item['link'] = re.sub(r'PHPSESSID=\w+',
+			"",
+			response.url)
+		keyinfo = response.xpath('//div[@class="keyinfo"][1]')
+		smalltext = keyinfo.xpath('div[@class="smalltext"]')
+		date = smalltext.xpath('text()').extract()[-1].replace(u'\u00bb','').strip()
+		if date.startswith("at"):
+			day = smalltext.xpath('strong[2]/text()').extract()[0]
+			if day=="Today":
+				today = datetime.now()
+			else:
+				today = datetime.now()-timedelta(hours=24)
+			at,time,am = date.split(" ")
+			time = [int(i) for i in time.split(":")]
+			if am=="PM":
+				time[0]+=12
+			today = today.replace(hour=time[0],
+				minute=time[1],
+				second=time[2])
+
+			date = today.strftime("%B %d, %Y, %I:%M:%S %p")
+
+		item['date_posted'] = date
+
+
+		postbody = " ".join(response.xpath('//div[@class="post"][1]/div/text()').extract())
+		posthead = keyinfo.xpath('h5/a/text()').extract()[0]
+		post = posthead+postbody
+		item['brand']="Unknown"
+		for brand in self.brandchoices:
+			if re.search(brand,post,re.IGNORECASE):
+				item['brand']=brand
+
+		if self.forsale:
+			if re.search("(FS|FOR SALE)",post,re.IGNORECASE):
+				yield item
+		else:
+			yield item
